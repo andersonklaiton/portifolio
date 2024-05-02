f8857aa6fff72029e87e1c15e58484622650806b
Anderson Silva <klaito.b@gmail.com>
2024-05-02 14:31:15 -0300

    create css for sidebar

--- portifólio/src/components/Sidebar.jsx
+++ portifólio/src/components/Sidebar.jsx
@@ -1,17 +1,17 @@
 import Avatar from "../img/avatar.jpeg"
 
-import "../styles/components/sidebar.sass"
+import "../styles/components/sidebar.sass";
 
-const Sidebar = ()=>{
-    return(
-        <aside id="sidebar">
-            <img src={Avatar} alt="Anderson Silva"/>
-            <p className="tittle">Desenvolvedor</p>
-            <p>redes sociais</p>
-            <p>informações de contato</p>
-            <a href="" className="btn">Download Currículo</a>
-        </aside>
-    )
-}
-
-export default Sidebar
+const Sidebar = () => {
+    return (
+      <aside id="sidebar">
+        <img src={Avatar} alt="Anderson da Silva" />
+        <p className="title">Desenvolvedor</p>
+        <a href="#" className="btn">
+          Download currículo
+        </a>
+      </aside>
+    );
+  };
+  
+  export default Sidebar;
